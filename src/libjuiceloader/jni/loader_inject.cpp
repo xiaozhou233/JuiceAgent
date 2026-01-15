94d881fe636ef8ec299eba5e144fbf29046c95b8
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-01-15 14:36:55 +0800

    [Feat] AddToClassLoader/AddToSystemClassLoaderSearch/AddToBootstrapClassLoaderSearch

--- src/libjuiceloader/jni/loader_inject.cpp
+++ src/libjuiceloader/jni/loader_inject.cpp
@@ -1,5 +1,6 @@
 #include <jni_impl.h>
 
+// Deprecated
 JNIEXPORT jboolean JNICALL Java_cn_xiaozhou233_juiceloader_JuiceLoader_injectJar
 (JNIEnv *env, jclass loader_class, jstring jarPath) {
     PLOGI << "Invoked!";
@@ -27,7 +28,7 @@ JNIEXPORT jboolean JNICALL Java_cn_xiaozhou233_juiceloader_JuiceLoader_injectJar
     return JNI_TRUE;
 }
 
-
+// Deprecated
 JNIEXPORT jobject JNICALL Java_cn_xiaozhou233_juiceloader_JuiceLoader_nativeInjectJarToThread
  (JNIEnv* env, jclass, jobject threadObj, jstring jJarPath)
 {
@@ -87,4 +88,108 @@ JNIEXPORT jobject JNICALL Java_cn_xiaozhou233_juiceloader_JuiceLoader_nativeInje
     env->ReleaseStringUTFChars(jJarPath, cJarPath);
 
     return newClassLoader;
+}
+
+JNIEXPORT jboolean JNICALL Java_cn_xiaozhou233_juiceloader_JuiceLoader_AddToBootstrapClassLoaderSearch
+  (JNIEnv *env, jclass, jstring jarPath) {
+    PLOGD << "Invoked!";
+    const char *pathStr = env->GetStringUTFChars(jarPath, NULL);
+    if (!pathStr) {
+        PLOGE << "Path is NULL!";
+        return JNI_FALSE;
+    }
+
+    if (!JuiceLoaderNative.jvmti) {
+        PLOGE.printf("JuiceLoaderNative.jvmti is NULL! [jvmti=%p]", JuiceLoaderNative.jvmti);
+        env->ReleaseStringUTFChars(jarPath, pathStr);
+        return JNI_FALSE;
+    }
+
+    jvmtiError result = JuiceLoaderNative.jvmti->AddToBootstrapClassLoaderSearch(pathStr);
+    if (result != JVMTI_ERROR_NONE) {
+        PLOGE.printf("Cannot AddToBootstrapClassLoaderSearch [result=%d]", result);
+        env->ReleaseStringUTFChars(jarPath, pathStr);
+        return JNI_FALSE;
+    }
+
+    env->ReleaseStringUTFChars(jarPath, pathStr);
+    PLOGI << "AddToBootstrapClassLoaderSearch: " << pathStr;
+    return JNI_TRUE;
+}
+
+JNIEXPORT jboolean JNICALL Java_cn_xiaozhou233_juiceloader_JuiceLoader_AddToSystemClassLoaderSearch
+  (JNIEnv *env, jclass, jstring jarPath) {
+    PLOGD << "Invoked!";
+    const char *pathStr = env->GetStringUTFChars(jarPath, NULL);
+    if (!pathStr) {
+        PLOGE << "Path is NULL!";
+        return JNI_FALSE;
+    }
+
+    if (!JuiceLoaderNative.jvmti) {
+        PLOGE.printf("JuiceLoaderNative.jvmti is NULL! [jvmti=%p]", JuiceLoaderNative.jvmti);
+        env->ReleaseStringUTFChars(jarPath, pathStr);
+        return JNI_FALSE;
+    }
+
+    jvmtiError result = JuiceLoaderNative.jvmti->AddToSystemClassLoaderSearch(pathStr);
+    if (result != JVMTI_ERROR_NONE) {
+        PLOGE.printf("Cannot AddToSystemClassLoaderSearch [result=%d]", result);
+        env->ReleaseStringUTFChars(jarPath, pathStr);
+        return JNI_FALSE;
+    }
+
+    env->ReleaseStringUTFChars(jarPath, pathStr);
+    PLOGI << "AddToSystemClassLoaderSearch: " << pathStr;
+    return JNI_TRUE;
+}
+
+JNIEXPORT jboolean JNICALL
+Java_cn_xiaozhou233_juiceloader_JuiceLoader_AddToClassLoader
+    (JNIEnv *env, jclass, jstring path, jobject loader) {
+
+    jclass urlClassLoader = env->FindClass("java/net/URLClassLoader");
+
+    if (env->IsInstanceOf(loader, urlClassLoader)) {
+        jclass fileClass = env->FindClass("java/io/File");
+        jmethodID fileInit = env->GetMethodID(
+                fileClass,
+                "<init>",
+                "(Ljava/lang/String;)V"
+        );
+
+        jobject file = env->NewObject(fileClass, fileInit, path);
+
+        jmethodID toURI = env->GetMethodID(
+                fileClass,
+                "toURI",
+                "()Ljava/net/URI;"
+        );
+        jobject uri = env->CallObjectMethod(file, toURI);
+
+        jclass uriClass = env->FindClass("java/net/URI");
+        jmethodID toURL = env->GetMethodID(
+                uriClass,
+                "toURL",
+                "()Ljava/net/URL;"
+        );
+        jobject url = env->CallObjectMethod(uri, toURL);
+
+        jmethodID addURL = env->GetMethodID(
+                urlClassLoader,
+                "addURL",
+                "(Ljava/net/URL;)V"
+        );
+        env->CallVoidMethod(loader, addURL, url);
+    } else {
+        // jstring -> const char*
+        const char* nativePath = env->GetStringUTFChars(path, nullptr);
+
+        JuiceLoaderNative.jvmti->AddToSystemClassLoaderSearch(nativePath);
+
+        // Release UTF string
+        env->ReleaseStringUTFChars(path, nativePath);
+    }
+
+    return JNI_TRUE;
 }
