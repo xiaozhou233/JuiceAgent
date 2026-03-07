172f354b049fa9a56e01ce8ba11e232ba3946b05
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-03-07 16:41:20 +0800

    [Refactor] Config

--- includes/JuiceAgent/JuiceAgent.h
+++ includes/JuiceAgent/JuiceAgent.h
@@ -4,13 +4,14 @@
 #define INJECT_PATH_MAX 260
 
 typedef struct _InjectionInfo {
-    char BootstrapAPIPath[INJECT_PATH_MAX];
-    char JuiceLoaderJarPath[INJECT_PATH_MAX];
+    char JuiceAgentAPIJarPath[INJECT_PATH_MAX];
+    char JuiceAgentNativePath[INJECT_PATH_MAX];
+
     char EntryJarPath[INJECT_PATH_MAX];
     char EntryClass[INJECT_PATH_MAX];
     char EntryMethod[INJECT_PATH_MAX];
+
     char InjectionDir[INJECT_PATH_MAX];
-    char JuiceLoaderLibraryPath[INJECT_PATH_MAX];
 } InjectionInfoType;
 
 extern InjectionInfoType InjectionInfo;
