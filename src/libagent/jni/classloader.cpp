4496931bdf4e0d1575aa034856b3d654c0a0ccfe
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-04-01 10:36:39 +0800

    Impl: addToClassLoader

--- src/libagent/jni/classloader.cpp
+++ src/libagent/jni/classloader.cpp
@@ -50,3 +50,123 @@ JNIEXPORT jboolean JNICALL Java_cn_xiaozhou233_juiceagent_api_JuiceAgent_addToSy
     env->ReleaseStringUTFChars(jar_path, j_jar_path);
     return JNI_TRUE;
   }
+
+JNIEXPORT jboolean JNICALL Java_cn_xiaozhou233_juiceagent_api_JuiceAgent_addToClassLoader
+  (JNIEnv *env, jclass, jstring jar_path, jobject target_classloader) {
+
+    auto& agent = JuiceAgent::Agent::instance();
+
+    if (!check_env(agent))
+        return JNI_FALSE;
+
+    if (!jar_path || !target_classloader)
+        return JNI_FALSE;
+
+    // --- Try URLClassLoader path ---
+    jclass urlClassLoader = env->FindClass("java/net/URLClassLoader");
+    if (env->ExceptionCheck() || !urlClassLoader) {
+        env->ExceptionClear();
+        goto FALLBACK;
+    }
+
+    if (env->IsInstanceOf(target_classloader, urlClassLoader)) {
+
+        jclass fileClass = env->FindClass("java/io/File");
+        if (env->ExceptionCheck() || !fileClass) {
+            env->ExceptionClear();
+            goto FALLBACK;
+        }
+
+        jmethodID fileInit = env->GetMethodID(fileClass, "<init>", "(Ljava/lang/String;)V");
+        if (env->ExceptionCheck() || !fileInit) {
+            env->ExceptionClear();
+            goto FALLBACK;
+        }
+
+        jobject file = env->NewObject(fileClass, fileInit, jar_path);
+        if (env->ExceptionCheck() || !file) {
+            env->ExceptionClear();
+            goto FALLBACK;
+        }
+
+        jmethodID toURI = env->GetMethodID(fileClass, "toURI", "()Ljava/net/URI;");
+        if (env->ExceptionCheck() || !toURI) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            goto FALLBACK;
+        }
+
+        jobject uri = env->CallObjectMethod(file, toURI);
+        if (env->ExceptionCheck() || !uri) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            goto FALLBACK;
+        }
+
+        jclass uriClass = env->FindClass("java/net/URI");
+        if (env->ExceptionCheck() || !uriClass) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            env->DeleteLocalRef(uri);
+            goto FALLBACK;
+        }
+
+        jmethodID toURL = env->GetMethodID(uriClass, "toURL", "()Ljava/net/URL;");
+        if (env->ExceptionCheck() || !toURL) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            env->DeleteLocalRef(uri);
+            goto FALLBACK;
+        }
+
+        jobject url = env->CallObjectMethod(uri, toURL);
+        if (env->ExceptionCheck() || !url) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            env->DeleteLocalRef(uri);
+            goto FALLBACK;
+        }
+
+        jmethodID addURL = env->GetMethodID(urlClassLoader, "addURL", "(Ljava/net/URL;)V");
+        if (env->ExceptionCheck() || !addURL) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            env->DeleteLocalRef(uri);
+            env->DeleteLocalRef(url);
+            goto FALLBACK;
+        }
+
+        env->CallVoidMethod(target_classloader, addURL, url);
+        if (env->ExceptionCheck()) {
+            env->ExceptionClear();
+            env->DeleteLocalRef(file);
+            env->DeleteLocalRef(uri);
+            env->DeleteLocalRef(url);
+            goto FALLBACK;
+        }
+
+        // cleanup
+        env->DeleteLocalRef(file);
+        env->DeleteLocalRef(uri);
+        env->DeleteLocalRef(url);
+
+        return JNI_TRUE;
+    }
+
+FALLBACK:
+
+    // --- JVMTI fallback ---
+    const char* j_jar_path = env->GetStringUTFChars(jar_path, nullptr);
+    if (!j_jar_path)
+        return JNI_FALSE;
+
+    jvmtiError err = agent.get_jvmti()->AddToSystemClassLoaderSearch(j_jar_path);
+
+    env->ReleaseStringUTFChars(jar_path, j_jar_path);
+
+    if (err != JVMTI_ERROR_NONE) {
+        return JNI_FALSE;
+    }
+
+    return JNI_TRUE;
+}
