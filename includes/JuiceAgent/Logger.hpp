6564a0e0202084d75935d2dcf9b7b94e696d347d
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-04-22 21:37:23 +0800

    feat: Update logger to use ColorConsoleAppender for improved output

--- includes/JuiceAgent/Logger.hpp
+++ includes/JuiceAgent/Logger.hpp
@@ -3,15 +3,16 @@
 #include <plog/Log.h>
 #include <plog/Init.h>
 #include <plog/Formatters/TxtFormatter.h>
-#include <plog/Appenders/ConsoleAppender.h>
+#include <plog/Appenders/ColorConsoleAppender.h>
 #include <plog/Appenders/DebugOutputAppender.h>
+#include <plog/Appenders/RollingFileAppender.h>
 
 inline void InitLogger()
 {
-    static plog::ConsoleAppender<plog::TxtFormatter> consoleAppender;
+    static plog::ColorConsoleAppender<plog::TxtFormatter> colorConsoleAppender;
     static plog::DebugOutputAppender<plog::TxtFormatter> debugAppender;
 
-    plog::init(plog::debug, &consoleAppender).addAppender(&debugAppender);
+    plog::init(plog::debug, &colorConsoleAppender).addAppender(&debugAppender);
 
     PLOG_INFO << "Logger initialized: Console + Debug Output";
 }
