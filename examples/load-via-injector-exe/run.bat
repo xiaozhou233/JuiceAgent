fe64308942151503dbfc355711befe6e4ff5d2f7
xiaozhou233 <xiaozhou233@vip.qq.com>
2026-08-15 10:23:15 +0800

    DELETE: all code

--- examples/load-via-injector-exe/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-java -jar target.jar
-pause
--- includes/JuiceAgent/JuiceAgent.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-struct LoaderConfig {
-    std::string JuiceAgentNativeLibraryPath;
-
-    std::string RuntimeDir;
-};
